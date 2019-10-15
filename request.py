ea94811a0a267e85e33fdd7fdfd8b490680f1174
samarthm24 <samarthm241020@gmail.com>
2019-10-15 11:34:11 +0530

    updated test script

--- request.py
+++ request.py
@@ -6,6 +6,6 @@ string = fp.read()
 x = {'code': string,
      'language': 'py'}
 
-r = requests.post('http://localhost:5001/v1/test', data=json.dumps(x),
+r = requests.post('http://localhost:5001/v1/run_code', data=json.dumps(x),
                   headers={'Content-Type': 'application/json'})
 print(r.status_code, r.json())
