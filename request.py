55d458ca42953d340f5cd5050039c1526e4c59f7
samarthm24 <samarthm241020@gmail.com>
2019-10-18 11:14:00 +0530

    updated test file

--- request.py
+++ request.py
@@ -4,7 +4,11 @@ import json
 fp = open('../timeout.py', 'r')
 string = fp.read()
 x = {'code': string,
-     'language': 'py'}
+     'language': 'py',
+     'problem_id': 'p1',
+     'student_id': 'sharath',
+     'contest_id': 'c1',
+     'time_out': 2}
 
 r = requests.post('http://localhost:5001/v1/run_code', data=json.dumps(x),
                   headers={'Content-Type': 'application/json'})
